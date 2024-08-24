c0e89d91736ced955d4ba370eb896659dd7903fd
nazrinayesha <168328634+nazrinayesha@users.noreply.github.com>
2024-08-24 20:27:23 +0530

    Update app.routes.ts

--- app.routes.ts
+++ app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MyTableComponent } from './my-table/my-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MyTableComponent },
+];
